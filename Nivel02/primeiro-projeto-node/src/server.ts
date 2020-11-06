690b308f58f6a5d0d9c47e532acb35f8296f068c
Sandro Valgoi <sandrovalgoi@gmail.com>
2020-11-05 23:08:55 -0300

    Início do GoBarber

--- Nivel02/primeiro-projeto-node/src/server.ts
+++ Nivel02/primeiro-projeto-node/src/server.ts
@@ -3,6 +3,7 @@ import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World com Prettier' });
