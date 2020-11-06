ca4e5e5e2667ddefe6a67d0216bfbb62b65425d2
Sandro Valgoi <sandrovalgoi@gmail.com>
2020-11-05 22:35:03 -0300

    Inclusão do debugger

--- Nivel02/primeiro-projeto-node/src/server.ts
+++ Nivel02/primeiro-projeto-node/src/server.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) => {
-  response.json({ message: 'Hello World com Prettier' });
+  return response.json({ message: 'Hello World com Prettier' });
 });
 
 const port = 3333;
