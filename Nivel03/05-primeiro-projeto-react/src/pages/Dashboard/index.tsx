88e96f967d32f6e843acfd8332372401252ad6ca
Sandro Valgoi <sandrovalgoi@gmail.com>
2020-11-14 21:48:58 -0300

    Styled Components

--- Nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
 
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
 const Dashboard: React.FC = () => {
   return (
     <>
-      <div>
-        <h1>Dashboard</h1>
-        <h2>Aqui</h2>
-      </div>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="button">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/3912977?s=460&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>teste/repository</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/3912977?s=460&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>teste/repository</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/3912977?s=460&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>teste/repository</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
