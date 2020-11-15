fa909e42cd522fce96a93cd0bb582b964c5bb473
Sandro Valgoi <sandrovalgoi@gmail.com>
2020-11-14 23:38:21 -0300

    Route params

--- Nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useEffect, useState } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 
@@ -86,7 +87,10 @@ const Dashboard: React.FC = () => {
       {inputError && <InputError>{inputError}</InputError>}
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -96,7 +100,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
