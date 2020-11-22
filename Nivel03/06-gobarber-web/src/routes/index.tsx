067fafc5b6a6a87976e5ae9744a76b25f7c367eb
Sandro Valgoi <sandrovalgoi@gmail.com>
2020-11-22 17:36:54 -0300

    Private Routes

--- Nivel03/06-gobarber-web/src/routes/index.tsx
+++ Nivel03/06-gobarber-web/src/routes/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-
-// import { Container } from './styles';
+import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
