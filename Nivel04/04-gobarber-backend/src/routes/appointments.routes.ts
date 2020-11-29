1f90717c87565965f1f0c3cef90278b196dedafc
Sandro Valgoi <sandrovalgoi@gmail.com>
2020-11-29 16:02:19 -0300

    Level 4 starting

---
+++ Nivel04/04-gobarber-backend/src/routes/appointments.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import AppointmentsRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+appointmentsRouter.use(ensureAuthenticated);
+
+appointmentsRouter.get('/', async (_request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  return response.json(await appointmentsRepository.find());
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  const { provider_id, date } = request.body;
+  const parsedDate = parseISO(date);
+  const createAppointmentService = new CreateAppointmentService();
+
+  const appointment = await createAppointmentService.execute({
+    date: parsedDate,
+    provider_id,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
---
+++ Nivel04/04-gobarber-backend/src/routes/users.routes.ts
@@ -0,0 +1,47 @@
+import { Router } from 'express';
+import multer from 'multer';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import uploadConfig from '../config/upload';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (e) {
+    return response.status(400).json({ error: e.message });
+  }
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const { filename: avatarFilename } = request.file;
+
+    const { id: user_id } = request.user;
+
+    const user = await updateUserAvatar.execute({ user_id, avatarFilename });
+
+    delete user.password;
+
+    return response.json(user);
+  },
+);
+
+export default usersRouter;
