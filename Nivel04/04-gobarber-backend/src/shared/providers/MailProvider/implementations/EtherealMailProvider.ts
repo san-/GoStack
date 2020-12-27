444787e2209b33222ad17479696d310817f83d3a
Sandro Valgoi <sandrovalgoi@gmail.com>
2020-12-27 16:45:33 -0300

    Handlebars templates

--- Nivel04/04-gobarber-backend/src/shared/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ Nivel04/04-gobarber-backend/src/shared/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,10 +1,17 @@
+import IMailTemplateProvider from '@shared/providers/MailTemplateProvider/models/IMailTemplateProvider';
 import nodemailer, { Transporter } from 'nodemailer';
+import { inject, injectable } from 'tsyringe';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 import IMailProvider from '../models/IMailProvider';
 
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
     nodemailer
       .createTestAccount()
       .then(account => {
@@ -23,18 +30,24 @@ export default class EtherealMailProvider implements IMailProvider {
       });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail(mailMessage: ISendMailDTO): Promise<void> {
     if (!this.client) {
       setTimeout(async () => {
-        await this.sendMail(to, body);
+        await this.sendMail(mailMessage);
       }, 1000);
     } else {
+      const { to, subject, from, templateData } = mailMessage;
       const message = await this.client.sendMail({
-        from: 'GoBarber <gobarber@gobarber.com.br>',
-        to,
-        subject: 'Recuperação de senha',
-        text: body,
-        html: body,
+        from: {
+          name: from?.name || 'Equipe GoBarber',
+          address: from?.email || 'equipe@gobarber.com.br',
+        },
+        to: {
+          name: to.name,
+          address: to.email,
+        },
+        subject,
+        html: await this.mailTemplateProvider.parse(templateData),
       });
       console.log('Mail sent: %s', message.messageId);
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
--- Nivel04/04-gobarber-backend/src/shared/providers/index.ts
+++ Nivel04/04-gobarber-backend/src/shared/providers/index.ts
@@ -2,6 +2,8 @@ import { container } from 'tsyringe';
 
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTamplateProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
@@ -14,4 +16,9 @@ container.registerSingleton<IStorageProvider>(
 container.registerSingleton<IMailProvider>(
   'MailProvider',
   EtherealMailProvider,
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
 );
