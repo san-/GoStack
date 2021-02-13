b86de6b9833501ab70840716c6e386e1610fc727
Sandro Valgoi <sandrovalgoi@gmail.com>
2021-02-12 22:47:22 -0300

    Joi validation

--- Nivel04/04-gobarber-backend/src/modules/users/infra/http/routes/profile.routes.ts
+++ Nivel04/04-gobarber-backend/src/modules/users/infra/http/routes/profile.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProfileController from '../controllers/ProfileController';
+import { valid } from '@hapi/joi';
 
 const profileRouter = Router();
 const profileController = new ProfileController();
@@ -15,9 +16,17 @@ profileRouter.put(
     [Segments.BODY]: {
       name: Joi.string().required(),
       email: Joi.string().email().required(),
-      old_password: Joi.string(),
-      password: Joi.string(),
-      password_confirmation: Joi.string().valid(Joi.ref('password')),
+      old_password: Joi.string().allow(null).allow(''),
+      password: Joi.string().when('old_password', {
+        is: Joi.string(),
+        then: Joi.string().min(6).required(),
+        otherwise: Joi.string().allow(null).allow('')
+      }),
+      password_confirmation: Joi.string().when('old_password', {
+        is: Joi.string(),
+        then: Joi.string().min(6).required(),
+        otherwise: Joi.string().allow(null).allow('')
+      }).valid(Joi.ref('password')),
     },
   }),
   profileController.update,
